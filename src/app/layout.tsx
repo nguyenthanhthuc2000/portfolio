bd6434b33e3eb5a6b5c0e5de0833cc4d0b568d87
Thức Nguyễn <nguyenthanhthuc.2k@gmail.com>
2024-08-05 00:15:57 +0700

    next theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
+import ThemeProvider from '@/components/layout/ThemeProviders';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,7 +30,7 @@ export const metadata: Metadata = {
     'HTML5',
     'CSS3',
     'SCSS',
-    'Tailwind',
+    'TailwindCss',
     'Bootstrap',
     'Redux',
     'Nginx',
@@ -67,7 +70,11 @@ export default function RootLayout({
   return (
     <html lang="vi">
       <body>
-        {children}
+        <ThemeProvider>
+          <Header />
+          { children }
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ src/components/layout/ThemeProviders.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { ThemeProvider } from 'next-themes';
+import React from 'react';
+
+export default function ThemeProviders({ children }: { children: React.ReactNode }) {
+    return <ThemeProvider>{children}</ThemeProvider>
+}
