1390d65d50e1175869f1122a30472a634bbea4e9
Thức Nguyễn <nguyenthanhthuc.2k@gmail.com>
2024-08-05 23:59:33 +0700

    custom hooks: useWndowSize

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,9 +2,10 @@
 
 import { useEffect, useState } from 'react';
 import ThemeSwitcher from '@/components/general/ThemeSwitcher';
+import useWndowSize from '@/hooks/useWndowSize';
 
 const Header = () => {
-
+  const size = useWndowSize();
   return (
     <header>
       <ThemeSwitcher />
