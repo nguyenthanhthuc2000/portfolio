9aa14960a0f530691e5d27c58722a81587f84efb
Thức Nguyễn <nguyenthanhthuc.2k@gmail.com>
2024-08-20 00:10:53 +0700

    Author: nguyen.thuc Email: nguyenthanhthuc.2k@gmail.com Date: 2024/08/19 Sub: w header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
 
   return (
     <header className="w-full relative">
-      <div className="flex w-full backdrop-blur-sm max-w-7xl mx-auto items-center justify-between px-4 md:px-8 md:py-8 py-4 fixed top-0 left-0 right-0">
+      <div className="flex w-full backdrop-blur-sm max-w-7xl mx-auto items-center justify-between px-4 custom-width-header md:px-8 md:py-8 py-4 fixed top-0 left-0 right-0">
 
         <NextLink href="/">
           <Logo />
